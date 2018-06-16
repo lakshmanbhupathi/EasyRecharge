c7cbbfd42c39dc43f64bdb6c33b385bd537d4baf
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-06-16 23:14:09 +0530

    master: Added Test.java

--- EasyRecharge/src/com/lakshman/easyrecharge/Test.java
+++ EasyRecharge/src/com/lakshman/easyrecharge/Test.java
@@ -1,4 +1,11 @@
 package com.lakshman.easyrecharge;
 
+import com.lakshman.easyrecharge.controller.EasyRechargeCmdFrontController;
+import com.lakshman.easyrecharge.controller.EasyRechargeController;
+
 public class Test {
+    public static void main(String args[]) {
+        EasyRechargeController cmdFrontController = new EasyRechargeCmdFrontController();
+        cmdFrontController.startEasyRechargeJourney();
+    }
 }
